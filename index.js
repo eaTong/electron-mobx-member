ddc06323aaada0172e604bf2aa8303110b6e2b47
eaTong <eatongchou@gmail.com>
2018-06-22 23:21:57 +0800

    upgrade to webpack4

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
  * Created by eatong on 17-3-14.
  */
 const electron = require('electron');
-const {app, BrowserWindow} = require('electron');
+const {app, BrowserWindow, globalShortcut} = require('electron');
 const path = require('path');
 const url = require('url');
 const nodeEnv = process.env.NODE_ENV;
@@ -36,14 +36,14 @@ function createWindow() {
 
   if (nodeEnv === 'development') {
     //delay 2000ms to wait for webpack-dev-server start
-    setTimeout(function(){
+    setTimeout(function () {
       win.loadURL(url.format({
         pathname: "localhost:3000",
         protocol: 'http:',
         slashes: true
       }));
       win.webContents.openDevTools();
-    },2000);
+    }, 2000);
   } else {
     win.loadURL(url.format({
       pathname: path.join(path.resolve(__dirname, './dist'), 'index.html'),
@@ -51,4 +51,8 @@ function createWindow() {
       slashes: true
     }));
   }
+
+  globalShortcut.register('CommandOrControl+Shift+D', () => {
+    win.webContents.openDevTools();
+  })
 }
